2fcc198888297cb87a82a95792a591229cd07620
ysbhadoriya <155433287+ysbhadoriya@users.noreply.github.com>
2024-11-26 08:36:08 +0530

    Update products.js

--- api/products.js
+++ api/products.js
@@ -7,7 +7,7 @@ const data = [
       "price": 65000.00,
       "stock": 50,
       "description": "Powerful laptop with a quad-core i5 processor, 8GB RAM, 256GB SSD, and a 14-inch FHD display.",
-      "image": "../img/lapi.png"
+      "image": "./img/lapi.png"
     },
     {
       "id": 2,
@@ -16,7 +16,7 @@ const data = [
       "brand": "TechGadget",
       "price": 49999.00,
       "stock": 100,
-      "image": "../img/iphone.png",
+      "image": "./img/iphone.png",
       "description": "Feature-rich smartphone with a 6.2-inch screen, 12MP dual camera, 128GB storage, and a 4000mAh battery."
     },
     {
@@ -26,7 +26,7 @@ const data = [
       "brand": "SoundBeats",
       "price": 500.00,
       "stock": 30,
-      "image": "../img/headphone.png",
+      "image": "./img/headphone.png",
       "description": "High-quality wireless headphones with over-ear design, 20 hours of battery life, and a sleek black color."
     },
     {
@@ -36,7 +36,7 @@ const data = [
       "brand": "FitTech",
       "price": 800.00,
       "stock": 20,
-      "image": "../img/watch.png",
+      "image": "./img/watch.png",
       "description": "Smartwatch with a 1.3-inch AMOLED display, water-resistant design, fitness tracking features, and a stylish silver color."
     },
     {
@@ -46,7 +46,7 @@ const data = [
       "brand": "SoundBeats",
       "price": 1100.00,
       "stock": 30,
-      "image": "../img/speakers.png",
+      "image": "./img/speakers.png",
       "description": "High-quality wireless headphones with over-ear design, 20 hours of battery life, and a sleek black color."
     },
     {
@@ -56,7 +56,7 @@ const data = [
       "brand": "Samsung",
       "price": 25000.00,
       "stock": 20,
-      "image": "../img/tv.png",
+      "image": "./img/tv.png",
       "description": "Smartwatch with a 1.3-inch AMOLED display, water-resistant design, fitness tracking features, and a stylish silver color."
     }
     
